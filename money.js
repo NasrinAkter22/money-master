a49b710363c502c55b7f39c2802f5e0329f1f223
NasrinAkter22 <258nasrinakter@gmail.com>
2022-02-17 20:48:13 +0600

    final section

--- money.js
+++ money.js
@@ -37,39 +37,3 @@ document.getElementById('save-button').addEventListener('click',function(){
    
    } )
 
-// error sight
-code // handle calculate button 
-document.getElementById('calculate-button').addEventListener('click', function () {
-    calculate()
-     
-})
-//income filed 
-function calculate() {
-    const incomeInput = document.getElementById("income-filed").value;
-    const incomeText = parseFloat(incomeInput);
-   // console.log(incomeText);
-
-    // expense
-
-    const foodInput = document.getElementById('food-filed').value;
-    const rentInput = document.getElementById('rent-filed').value;
-    const clothesInput = document.getElementById('clothes-filed').value;
-
-    const foodInputNumber = parseFloat(foodInput);
-    const rentInputNumber = parseFloat(rentInput)
-    const clothesInputNumber = parseFloat(clothesInput);
-
-
-    //error
-    if(foodInputNumber>0 && typeof foodInputNumber=='number' && rentInputNumber>0 && typeof rentInputNumber=='number' && clothesInputNumber>0 && typeof clothesInputNumber=='number'){
-    // calculate
-    const calculateTotal =foodInputNumber+ rentInputNumber+ clothesInputNumber;
-    ///calculate balance Total
-    const balanceTotal = incomeText - calculateTotal; 
-    document.getElementById('balance').innerText = balanceTotal;
-    document.getElementById("total-expense").innerText = calculateTotal
-
-    }else{
-        alert('Type the number')
-    }
-
