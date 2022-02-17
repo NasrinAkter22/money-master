9dc8d4e3899395ae77fc49a13a152ab4f4548f86
NasrinAkter22 <258nasrinakter@gmail.com>
2022-02-17 20:44:06 +0600

    function work

--- money.js
+++ money.js
@@ -23,12 +23,53 @@ function calculate() {
     document.getElementById("total-expense").innerText = calculateTotal
 }
 // save button
-document.getElementById('save-button').addEventListener('click',
-function(){
+document.getElementById('save-button').addEventListener('click',function(){
+   
     const incomeInput=document.getElementById('income-filed');
-    const savingInput=document.getElementById('saving-input');
-    const savingAmount=(parseFloat(incomeInput.valu)*parseFloat(savingInput.value))/100;
-    // document.getElementById('saving-amount').innerText=savingAmount;
-    const balance=document.getElementById('balance').innerText;
+    const savingInput=document.getElementById('save-input');
+    const savingAmount=(parseFloat(incomeInput.value)*parseFloat(savingInput.value))/100;
+    document.getElementById('saving-amount').innerText=savingAmount;
+   
+    const balance=document.getElementById('balance');
+   
+    const remaingBalance=parseFloat(balance.innerText)-savingAmount;
+    document.getElementById('remaing-balance').innerText=remaingBalance;
+   
+   } )
 
-} )
+// error sight
+code // handle calculate button 
+document.getElementById('calculate-button').addEventListener('click', function () {
+    calculate()
+     
+})
+//income filed 
+function calculate() {
+    const incomeInput = document.getElementById("income-filed").value;
+    const incomeText = parseFloat(incomeInput);
+   // console.log(incomeText);
+
+    // expense
+
+    const foodInput = document.getElementById('food-filed').value;
+    const rentInput = document.getElementById('rent-filed').value;
+    const clothesInput = document.getElementById('clothes-filed').value;
+
+    const foodInputNumber = parseFloat(foodInput);
+    const rentInputNumber = parseFloat(rentInput)
+    const clothesInputNumber = parseFloat(clothesInput);
+
+
+    //error
+    if(foodInputNumber>0 && typeof foodInputNumber=='number' && rentInputNumber>0 && typeof rentInputNumber=='number' && clothesInputNumber>0 && typeof clothesInputNumber=='number'){
+    // calculate
+    const calculateTotal =foodInputNumber+ rentInputNumber+ clothesInputNumber;
+    ///calculate balance Total
+    const balanceTotal = incomeText - calculateTotal; 
+    document.getElementById('balance').innerText = balanceTotal;
+    document.getElementById("total-expense").innerText = calculateTotal
+
+    }else{
+        alert('Type the number')
+    }
+
