e204f90d21989094c9098ed3dbcb1676d09fa5c0
NasrinAkter22 <258nasrinakter@gmail.com>
2022-02-17 21:12:43 +0600

    complete sight

--- money.js
+++ money.js
@@ -1,4 +1,4 @@
-// handle calculate button 
+ // handle calculate button 
 document.getElementById('calculate-button').addEventListener('click', function () {
     calculate()
      
@@ -14,26 +14,48 @@ function calculate() {
     const foodInput = document.getElementById('food-filed').value;
     const rentInput = document.getElementById('rent-filed').value;
     const clothesInput = document.getElementById('clothes-filed').value;
+
+    const foodInputNumber = parseFloat(foodInput);
+    const rentInputNumber = parseFloat(rentInput)
+    const clothesInputNumber = parseFloat(clothesInput);
+
+
+    //error
+    if(foodInputNumber>0 && typeof foodInputNumber=='number' && rentInputNumber>0 && typeof rentInputNumber=='number' && clothesInputNumber>0 && typeof clothesInputNumber=='number'){
     // calculate
-    const calculateTotal =
-        parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothesInput);
+    const calculateTotal =foodInputNumber+ rentInputNumber+ clothesInputNumber;
     ///calculate balance Total
     const balanceTotal = incomeText - calculateTotal; 
     document.getElementById('balance').innerText = balanceTotal;
     document.getElementById("total-expense").innerText = calculateTotal
+
+    }else{
+        alert('Type the number')
+    }
+
+
+  
 }
 // save button
 document.getElementById('save-button').addEventListener('click',function(){
    
-    const incomeInput=document.getElementById('income-filed');
-    const savingInput=document.getElementById('save-input');
-    const savingAmount=(parseFloat(incomeInput.value)*parseFloat(savingInput.value))/100;
-    document.getElementById('saving-amount').innerText=savingAmount;
-   
-    const balance=document.getElementById('balance');
-   
-    const remaingBalance=parseFloat(balance.innerText)-savingAmount;
-    document.getElementById('remaing-balance').innerText=remaingBalance;
-   
-   } )
+ const incomeInput=document.getElementById('income-filed');
+ const savingInput=document.getElementById('save-input');
+ const savingAmount=(parseFloat(incomeInput.value)*parseFloat(savingInput.value))/100;
 
+ const balance=document.getElementById('balance');
+
+
+ //error
+ 
+ if(parseInt(balance.innerText)>savingAmount){
+    document.getElementById('saving-amount').innerText=savingAmount;
+    const remaingBalance=parseFloat(balance.innerText)-savingAmount;
+
+    document.getElementById('remaing-balance').innerText=remaingBalance;
+
+}else{
+    alert('You do not have enough money to save')
+}
+
+} )
